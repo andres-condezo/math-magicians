00aba201539abba3c4c20661d77c67b75844b6a3
andres-condezo <ra.condezomonge@gmail.com>
2022-02-11 11:22:58 -0500

    Feat(calculator.css): [Add a handle click function]

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,35 +1,51 @@
 import React, { Component } from 'react';
 import './Calculator.css';
+import calculate from './logic/calculate';
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(btn) {
+    const btnValue = btn.target.textContent;
+    this.setState((e) => calculate(e, btnValue));
   }
 
   render() {
+    const { total, next, operation } = this.state;
     return (
       <div className="container">
-        <div className="screen">0</div>
-        <button type="button" className="btn">AC</button>
-        <button type="button" className="btn">+/-</button>
-        <button type="button" className="btn">%</button>
-        <button type="button" className="btn orange">÷</button>
-        <button type="button" className="btn">7</button>
-        <button type="button" className="btn">8</button>
-        <button type="button" className="btn">9</button>
-        <button type="button" className="btn orange">x</button>
-        <button type="button" className="btn">4</button>
-        <button type="button" className="btn">5</button>
-        <button type="button" className="btn">6</button>
-        <button type="button" className="btn orange">-</button>
-        <button type="button" className="btn">1</button>
-        <button type="button" className="btn">2</button>
-        <button type="button" className="btn">3</button>
-        <button type="button" className="btn orange">+</button>
-        <button type="button" className="btn zero">0</button>
-        <button type="button" className="btn">.</button>
-        <button type="button" className="btn orange">=</button>
+        <div className="screen">
+          { total }
+          { operation }
+          { next }
+        </div>
+        <button type="button" onClick={this.handleClick} className="btn">AC</button>
+        <button type="button" onClick={this.handleClick} className="btn">+/-</button>
+        <button type="button" onClick={this.handleClick} className="btn">%</button>
+        <button type="button" onClick={this.handleClick} className="btn orange">÷</button>
+        <button type="button" onClick={this.handleClick} className="btn">7</button>
+        <button type="button" onClick={this.handleClick} className="btn">8</button>
+        <button type="button" onClick={this.handleClick} className="btn">9</button>
+        <button type="button" onClick={this.handleClick} className="btn orange">x</button>
+        <button type="button" onClick={this.handleClick} className="btn">4</button>
+        <button type="button" onClick={this.handleClick} className="btn">5</button>
+        <button type="button" onClick={this.handleClick} className="btn">6</button>
+        <button type="button" onClick={this.handleClick} className="btn orange">-</button>
+        <button type="button" onClick={this.handleClick} className="btn">1</button>
+        <button type="button" onClick={this.handleClick} className="btn">2</button>
+        <button type="button" onClick={this.handleClick} className="btn">3</button>
+        <button type="button" onClick={this.handleClick} className="btn orange">+</button>
+        <button type="button" onClick={this.handleClick} className="btn zero">0</button>
+        <button type="button" onClick={this.handleClick} className="btn">.</button>
+        <button type="button" onClick={this.handleClick} className="btn orange">=</button>
       </div>
     );
   }
